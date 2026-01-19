032b18fd494483b08398d7df5f4061a7a03221a3
Konsta Niinimaki <konsta.niinimaki@servur.org>
2026-01-19 13:47:38 +0200

    Update to version 0.2.2

--- TrackingSwitch_X.lua
+++ TrackingSwitch_X.lua
@@ -2,7 +2,11 @@ TrackingSwitch_X = LibStub("AceAddon-3.0"):NewAddon("TrackingSwitch_X", "AceCons
 
 local defaults = {
   profile = {
+    lastNoticeVersion = 0,
     enableOnLogin = false,
+    muteSwitchSound = false,
+    disableWhileTargetActive = false,
+    disableWhileCursorActive = true,
     disableStationary = true,
     disableResting = true,
     disableCombat = true,
@@ -54,6 +58,28 @@ local options = {
       name = " ", 
       type = "description",
       order = 3,
+    },
+    disableWhileTargetActive = {
+      name = "Disable tracking switch while (attackable) target active.",
+      desc = "Check to disable switching while (attackable) target is active.",
+      type = "toggle",
+      get = function() return TrackingSwitch_X.db.profile.disableWhileTargetActive end,
+      set = function(_, value)
+        TrackingSwitch_X.db.profile.disableWhileTargetActive = value
+        TrackingSwitch_X:UpdateTimerInterval()
+      end,
+      width = "full",
+    },
+    disableWhileCursorActive = {
+      name = "Disable tracking switch while dragging spell/item",
+      desc = "Check to disable switching while dragging a spell or item.",
+      type = "toggle",
+      get = function() return TrackingSwitch_X.db.profile.disableWhileCursorActive end,
+      set = function(_, value)
+        TrackingSwitch_X.db.profile.disableWhileCursorActive = value
+        TrackingSwitch_X:UpdateTimerInterval()
+      end,
+      width = "full",
     },
     disableStationary = {
       name = "Disable tracking switch while stationary.",
@@ -109,6 +135,7 @@ local options = {
       get = function() return TrackingSwitch_X.db.profile.useCustomTracking end,
       set = function(_, value)
         TrackingSwitch_X.db.profile.useCustomTracking = value
+        TrackingSwitch_X:RebuildTrackingList()
         TrackingSwitch_X:UpdateTimerInterval()
       end,
       width = "full",
@@ -185,6 +212,13 @@ local options = {
 
 local AceGUI = LibStub("AceGUI-3.0")
 
+-- Map spell names to spell IDs for language-independent casting
+local spellNameToID = {
+  ["Find Minerals"] = 2580,
+  ["Find Herbs"] = 2383,
+  ["Find Treasure"] = 2481,
+  ["Find Fish"] = 43308,
+}
 
 function TrackingSwitch_X:RebuildTrackingList()
   wipe(self.trackingList)
@@ -212,6 +246,14 @@ function TrackingSwitch_X:OnInitialize()
 
 
   print("Type /ts to toggle or /tso, /tsx for options")
+  if self.db.profile.lastNoticeVersion < 1 then
+    C_Timer.After(5, function()
+      DEFAULT_CHAT_FRAME:AddMessage("|CFF00FFFFNew options added!|r")
+      DEFAULT_CHAT_FRAME:AddMessage("|CFF00FFFFDisable tracking while target active or while moving items. Also mute switch sound.|r")
+      DEFAULT_CHAT_FRAME:AddMessage("|CFF00FFFF/tsx for options!|r")
+      self.db.profile.lastNoticeVersion = 1
+    end)
+  end
   LibStub("AceConfig-3.0"):RegisterOptionsTable("TrackingSwitch_X", options)
 
   self.optionsFrame = LibStub('AceConfigDialog-3.0'):AddToBlizOptions('TrackingSwitch_X', 'TrackingSwitch_X')
@@ -265,17 +307,26 @@ function TrackingSwitch_X:SwitchTracking()
     if (not self.db.profile.disableStationary or IsPlayerMoving()) and
        (not self.db.profile.disableResting or not IsResting()) and
        (not self.db.profile.disableCombat or not UnitAffectingCombat("player")) and
-       (not UnitChannelInfo("player")) then
+       (not self.db.profile.disableWhileTargetActive or not UnitCanAttack("player", "target")) and
+       (not self.db.profile.disableWhileCursorActive or not GetCursorInfo()) and
+       (not UnitChannelInfo("player"))
+       then
 
         local function castSpell(spellName)
+            local spellID = spellNameToID[spellName]
+            if not spellID then
+                print("Unknown spell: " .. spellName)
+                return
+            end
+            
             if self.db.profile.muteSwitchSound then
                 -- temporarily mute
                 local oldVolume = GetCVar("Sound_EnableSFX")
                 SetCVar("Sound_EnableSFX", 0)
-                CastSpellByName(spellName)
+                CastSpellByID(spellID)
                 SetCVar("Sound_EnableSFX", oldVolume)
             else
-                CastSpellByName(spellName)
+                CastSpellByID(spellID)
             end
         end
 
